42754663dc8d480be6b6f32ac6f9cd58e1ca1c37
Strencher <46447572+Strencher@users.noreply.github.com>
2020-06-24 18:44:25 +0200

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {
     webpack: {
         getModule,
         getModuleByDisplayName,
+        getAllModules,
         React
     },
     injector: {
@@ -14,39 +15,47 @@ const {
 } = require("powercord");
 const ImageModal = getModuleByDisplayName("ImageModal", false);
 const { MenuItem, MenuGroup } = getModule(m => m.MenuRadioItem && !m.default, false);
-const GuildChannelUserContextMenu = getModule(m => m.default && m.default.displayName === 'GuildChannelUserContextMenu', false);
 const ModalStack = getModule(["push", "popAll"], false);
-const copy = e=>getModule(["copy"], false).copy(e);
+const AllUserContextMenus = getAllModules(m=>
+    m.default && 
+    m.default.displayName && 
+    /^(?=.*User)(?=.*ContextMenu)/i.test(m.default.displayName)
+, false);
+const copy = e => getModule(["copy"], false).copy(e);
+// Below Copied from ZLibrary
+const findInTree = (e,t) => t.split(".").reduce((e,p)=>e&&e[p],e);
 module.exports = class AvatarViewer extends Plugin {
     startPlugin() {
-        if(GuildChannelUserContextMenu) inject(this.entityID, GuildChannelUserContextMenu, "default", (args, ret) => {
-            const [props] = args;
-            if(!ret || !ret.props || !ret.props.children || !ret.props.children.props || !Array.isArray(ret.props.children.props.children)) return ret;
-            else ret.props.children.props.children.push(
-                React.createElement(MenuGroup, {
-                    children: [
-                        React.createElement(MenuItem, {
-                            id: "avatar-viewer-view",
-                            action: () => {
-                                this.openModal(props.user.getAvatarURL(getModule(['hasAnimatedAvatar'], false).hasAnimatedAvatar(props.user) ? "gif" : "webp").split("?")[0]+"?size=2048")
-                            },
-                            label: "View Avatar"
-                        }),
-                        React.createElement(MenuItem, {
-                            id: "avatar-viewer-copy",
-                            action: () => {
-                                copy(props.user.getAvatarURL().split("?")[0]+"?size=2048");
-                            },
-                            label: "Copy AvatarURL"
-                        })
-                    ]
-                })
-            )
-            return ret;
+        AllUserContextMenus.forEach(e=>{
+            const displayName = e.default.displayName;
+            inject(this.entityID+e.default.displayName, e, "default", ([{user}], ret) => {
+                const children = findInTree(ret, "props.children.props.children");
+                if(!Array.isArray(children)) return ret;
+                else children.push(
+                    React.createElement(MenuGroup, {
+                        children: [
+                            React.createElement(MenuItem, {
+                                id: "avatar-viewer-view",
+                                action: () => {
+                                    this.openModal(user.getAvatarURL(getModule(['hasAnimatedAvatar'], false).hasAnimatedAvatar(user) ? "gif" : "webp").split("?")[0]+"?size=2048")
+                                },
+                                label: "View Avatar"
+                            }),
+                            React.createElement(MenuItem, {
+                                id: "avatar-viewer-copy",
+                                action: () => {
+                                    copy(user.getAvatarURL().split("?")[0]+"?size=2048");
+                                },
+                                label: "Copy AvatarURL"
+                            })
+                        ]
+                    })
+                )
+                return ret;
+            });
+            e.default.displayName = displayName;
         })
-        GuildChannelUserContextMenu.default.displayName = 'GuildChannelUserContextMenu';
     }
-
     openModal(url) {
         ModalStack.push(e => React.createElement(ImageModal, {
             ...e,
@@ -60,6 +69,6 @@ module.exports = class AvatarViewer extends Plugin {
     }
 
     pluginWillUnload() {
-        uninject(this.entityID);
+        AllUserContextMenus.forEach(e=>uninject(this.entityID+e.default.displayName))
     }
 }
